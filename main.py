acbfd34c895bb08ea8d201072c2453a5bad9dd94
lepers <benjamin.lepers@gmail.com>
2025-01-20 18:27:00 +0100

    paths headeach, removing path_static in the fast_app class

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from pathlib import Path
 hdrs=(picolink)
 
 path_data = Path("public/data")
-app, rt = fast_app(hdrs=hdrs, static_path=path_data)
+app, rt = fast_app(hdrs=hdrs)
 
 logo_large = Img(
-                src="/logos/logo2.jpg",  # Replace with your logo URL
+                src=path_data/"logos/logo2.jpg",  # Replace with your logo URL
                 alt="Logo",
                 style="position: absolute; top: 20px; right: 20px; width: 100px;",
                 cls="responsive-img"
                 )
 
 logo_small = Img(
-                src="/logos/logo2.jpg",  # Replace with your logo URL
+                src=path_data/"logos/logo2.jpg",  # Replace with your logo URL
                 alt="Logo",
                 style="position: absolute; top: 20px; right: 20px; width: 50px;",
                 cls="responsive-img"
@@ -37,7 +37,7 @@ def get():
                 P(A('Assurance', href='/assurance')),
                 P(A('Videos', href='/videos')),
                 P(A('Photos', href='/photos')), 
-                P(A('logos', href='/Logos')), 
+                P(A('logos', href='/logos')), 
                 logo_large,
                 style="padding-top: 10px;"
                )
@@ -261,7 +261,7 @@ def get():
             )
 @rt('/videos')
 def get():
-    video_file_name = "/videos/pav_ben.mp4"
+    video_file_name = path_data/"videos/pav_ben.mp4"
 
     return Titled(
             "Videos",
@@ -271,8 +271,6 @@ def get():
                        Source(src=video_file_name ,type="video/mp4"),
                        controls=True,
                        style="width: 100%; height: auto;",
-                       #width=1024,
-                       #height=768,
                         ),
                        Figcaption("Planche à voile presqu'ile de Giens, pointe de l'Esterelle, juin 2024")
                        ),
@@ -302,7 +300,7 @@ def get():
                 }"""
                 ),
             Figure(
-                Img(src="/photos/calculatrice2.jpg", alt="Example image", style="width: 100%; height: auto;"),
+                Img(src=path_data/"photos/calculatrice2.jpg", alt="Example image", style="width: 100%; height: auto;", cls="responsive-img"),
                 Figcaption("Ceci est une casio")
             ),
             P(A('Sommaire', href='/')),
@@ -312,7 +310,7 @@ def get():
     )
  
 
-@rt('/Logos')
+@rt('/logos')
 def get():
     return Titled(
             "Logos",
@@ -331,15 +329,15 @@ def get():
                     }"""
                     ),
                 Figure(
-                    Img(src="logos/logo1.jpg", alt="Example image", style="width: 100%; height: auto;", cls="responsive-img"),
+                    Img(src=path_data/"logos/logo1.jpg", alt="Example image", style="width: 100%; height: auto;", cls="responsive-img"),
                     Figcaption("Mon logo1")
                 ),
                 Figure(
-                    Img(src="logos/logo2.jpg", alt="Example image", style="width: 100%; height: auto;",cls="responsive-img"),
+                    Img(src=path_data/"logos/logo2.jpg", alt="Example image", style="width: 100%; height: auto;",cls="responsive-img"),
                     Figcaption("Mon logo2")
                 ),
                 Figure(
-                    Img(src="logos/logo3.jpg", alt="Example image", style="width: 100%; height: auto;",cls="responsive-img"),
+                    Img(src=path_data/"logos/logo3.jpg", alt="Example image", style="width: 100%; height: auto;",cls="responsive-img"),
                     Figcaption("Mon logo3")
                 ),
                 P(A('Sommaire', href='/')),
