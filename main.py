034b3779f6d14a4e8becf2bd62d2909be58376c1
lepers <benjamin.lepers@gmail.com>
2025-01-20 13:55:10 +0100

    remove slash in the path

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 hdrs=(picolink)
 cwd = Path.cwd()
 
-path_data = cwd/"data/"
+path_data = cwd/"data"
 #print(f"path_data:{path_data}")
 app, rt = fast_app(hdrs=hdrs, static_path=path_data)
 
